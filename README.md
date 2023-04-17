4b1039f887839a3f5bf4e8dc175f3fee2ece23c5
Hagen Glötter <github@dg7shg.de>
2023-04-17 02:35:44 +0200

    Update "manual"

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # network-device-overview
 what devices are in my network - a simple overview about all devices in the local network -
+
+Setup:
+* sudo apt install lft
+*  which lft
+* sudo chmod +s /usr/sbin/lft
--- index_nmap.php
+++ index_nmap.php
@@ -2,11 +2,13 @@
 # do not forget to set the sticky bit 
 # chmod +s /usr/sbin/lft
 $starttime = time();
+print("<table>")
 print shell_exec('for i in `seq 1 254`;do (LANG=C lft 192.168.4.$i | grep trace | grep \'(\' &);done | sed \'s#.* to \(\(.*\).fritz.box\) (\(.*\)):.*#<tr><td></td>' .
 	'<td><a href="http://\1" target="_blank">\2</a></td><td><a href="http://\3/" target="_blank">\3</a></td><td><a href="nmap-hostinfo.php?ip=\3" target="_blank">' .
 	'<img src="css/searchicon.png" alt="click for hostinfo"></a></td></tr>#\' | sort -f');
 $mytime = time() - $starttime;
 print("<tr><td colspan=\"4\">done in $mytime seconds</td></tr>\n");
+print("</table>")
 
 # tests
 #$cmd = 'nmap     -sP 192.168.4.0/24 | grep  "scan report" | cut -d " " -f5-6 | sort -df';
